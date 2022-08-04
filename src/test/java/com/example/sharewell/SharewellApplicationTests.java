b9d1b0c1200c87419b45c0e44318c2cca469e4c7
AndrePeng1 <andre.c.peng@gmail.com>
2022-08-03 21:59:26 -0700

    Set up Spring boot

---
+++ src/test/java/com/example/sharewell/SharewellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharewellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
